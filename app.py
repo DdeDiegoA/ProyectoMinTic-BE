7465a914ea7d10887ed521274bd59dd0f05343f8
Diego Arenas <diegoarenas111@gmail.com>
2022-08-29 11:10:22 -0500

    config y .env

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from unicodedata import name
 from flask import Flask, jsonify
+from flask_cors import CORS
+from dotenv import dotenv_values
+
+config = dotenv_values('.env')
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_word():
-    dictToReturn = {'message':'Hola mundirilijillo'}
+    dictToReturn = {'message': 'Hola mundirilijillo'}
     return jsonify(dictToReturn)
-    
+
+
+print(__name__)
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=config["PORT"], debug=True)
